0378a0fc24dd78d2d6bd8e68f12d0bebdec510d1
Pouya Rezaei <PouyaRezaie7@gmail.com>
2024-04-02 19:19:02 +0330

    feat: Add basic struct of the SignInScreen

--- lib/feature/auth/sign_in/sign_in_screen.dart
+++ lib/feature/auth/sign_in/sign_in_screen.dart
@@ -1,8 +1,12 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:rounded_loading_button_plus/rounded_loading_button.dart';
 
 class SignInScreen extends StatelessWidget {
-
-  const SignInScreen({Key? key,}) : super(key: key);
+  const SignInScreen({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -11,17 +15,48 @@ class SignInScreen extends StatelessWidget {
 }
 
 class _SignInScreenContent extends StatelessWidget {
+  final RoundedLoadingButtonController _btnController =
+      RoundedLoadingButtonController();
+
+  void _doSomething() async {
+    Timer(const Duration(seconds: 3), () {
+      _btnController.success();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        TextField(
-          decoration: InputDecoration(
-            label: Text("Email"),
-
-          ),
-        )
-      ],
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            const TextField(
+              decoration: InputDecoration(
+                label: Text("Email"),
+              ),
+            ),
+            const TextField(
+              decoration: InputDecoration(
+                label: Text(
+                  "Password",
+                ),
+              ),
+            ),
+            RoundedLoadingButton(
+              controller: _btnController,
+              onPressed: _doSomething,
+              elevation: 0,
+              child: Text(
+                "Sign In",
+                style: Theme.of(context).textTheme.labelMedium?.copyWith(
+                    color: Theme.of(context).colorScheme.onPrimary
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
