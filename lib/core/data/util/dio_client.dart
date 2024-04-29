d089cf8b1956ffd33ceec96d6c046d1836c95a2a
Arman <shadowofarman@hotmail.com>
2024-04-29 23:11:12 +0330

    Fixed web version issue

--- lib/core/data/util/dio_client.dart
+++ lib/core/data/util/dio_client.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:dio/dio.dart';
 import 'package:dio/io.dart';
+import 'package:flutter/foundation.dart';
 
 class DioClient {
   final Dio dio = Dio();
@@ -15,7 +16,18 @@ class DioClient {
       'Access-Control-Allow-Origin': '*',
     };
 
-    (dio.httpClientAdapter as IOHttpClientAdapter).createHttpClient = ()  => HttpClient()
-      ..badCertificateCallback = (X509Certificate cert, String host, int port) => true;
+    if (kIsWeb) {
+      //TODO: Add addaptor for web client
+      
+      // (dio.httpClientAdapter as HttpClientAdapter).createHttpClient = () =>
+      //     HttpClient()
+      //       ..badCertificateCallback =
+      //           (X509Certificate cert, String host, int port) => true;
+    } else {
+      (dio.httpClientAdapter as IOHttpClientAdapter).createHttpClient = () =>
+          HttpClient()
+            ..badCertificateCallback =
+                (X509Certificate cert, String host, int port) => true;
+    }
   }
 }
