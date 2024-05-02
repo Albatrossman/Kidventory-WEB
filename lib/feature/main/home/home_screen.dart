1635eb7dbf3bfbe389cacdab4f8beeb25aba496e
Arman <shadowofarman@hotmail.com>
2024-05-02 22:11:48 +0330

    added refresh on home screen didPopNext

--- lib/feature/main/home/home_screen.dart
+++ lib/feature/main/home/home_screen.dart
@@ -55,7 +55,7 @@ class _HomeScreenState extends State<HomeScreen>
 
   @override
   void didPopNext() {
-    // _viewModel.updateContent();
+    _viewModel.updateContent();
     super.didPopNext();
   }
 
