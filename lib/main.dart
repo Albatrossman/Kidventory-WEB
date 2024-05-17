207bc4148b09373d699e22021df108142613abc4
Arman <shadowofarman@hotmail.com>
2024-05-17 13:29:51 +0330

    Fixed web name

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Kidventory',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 28, 176, 245)),
         useMaterial3: true,
